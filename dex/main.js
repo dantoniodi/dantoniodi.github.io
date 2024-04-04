cf7462b689e5df90818eb78365fad607cc746e6d
Diego <diego_dnas@hotmail.com>
2024-04-04 15:04:32 -0300

    Sprite Fix

--- dex/main.js
+++ dex/main.js
@@ -1,7 +1,4 @@
-var chosenGen = 1;
-var chosenType = 0;
-var chosenView = 'gif';
-var chosenForm = 'default';
+//global selectors
 const pokeContainer = document.querySelector('.container');
 const optGen = document.querySelector('#optGen');
 const optType = document.querySelector('#optType');
@@ -10,42 +7,44 @@ const optView = document.querySelector('#optView');
 const orderBy = document.querySelector('#orderBy');
 const totalPkm = document.querySelector('.total');
 
+//global variables
+var chosenGen = optGen.selectedIndex;
+var chosenType = 0;
+var chosenView = optView.checked ? 'animated' : 'static';
+var chosenForm = optForm.checked ? 'shiny' : 'default';
+
+//listeners
 optGen.addEventListener('change', (event) => {
     if(event.target.value != chosenGen) {
-        while (pokeContainer.firstChild) pokeContainer.removeChild(pokeContainer.firstChild)
         chosenGen = event.target.value
-        optType.selectedIndex = 0
-        chosenType = 0
+        pokeContainer.replaceChildren()
+        chosenType = optType.selectedIndex = 0
         getByGen(chosenGen)
     }
-})
+});
 
 optType.addEventListener('change', (event) => {
     if(event.target.value != chosenType) {
-        while (pokeContainer.firstChild) pokeContainer.removeChild(pokeContainer.firstChild)
         chosenType = event.target.value
-        optGen.selectedIndex = 0
-        chosenGen = 0
+        pokeContainer.replaceChildren()
+        chosenGen = optGen.selectedIndex = 0
         getByType(chosenType)
     }
-})
-
+});
 optForm.addEventListener("change", function(event) {
-    pokeContainer.innerHTML = ''
+    pokeContainer.replaceChildren()
     chosenForm = event.target.checked ? event.target.value : 'default'
     chosenType != 0 ? getByType(chosenType) : getByGen(chosenGen)
-})
-
+});
 optView.addEventListener("change", function(event) {
-    pokeContainer.innerHTML = ''
+    pokeContainer.replaceChildren()
     chosenView = event.target.checked ? event.target.value : 'png'
     chosenType != 0 ? getByType(chosenType) : getByGen(chosenGen)
-})
-
+});
 orderBy.addEventListener("change", function(event) {
-    pokeContainer.innerHTML = ''
+    pokeContainer.replaceChildren()
     chosenType != 0 ? getByType(chosenType) : getByGen(chosenGen)
-})
+});
 
 //unused just for info
 const pokeGen = {
@@ -97,12 +96,13 @@ const getByType = async(id) => {
 }
 
 const fetchPokemons = (list) => {
+    console.log(list);
     if (Array.isArray(list)) {
         if(orderBy.value == 'name') {
             list.sort((a,b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0))
         }
         list.forEach(pkm => {
-            let pid = pkm.url.includes('pokemon-species') ? pkm.name : pkm.url.split('/')[6];
+            let pid = pkm.url.includes('pokemon-species') ? pkm.url.split('/')[6] : pkm.name;
             fetch(getUrl(pid))
             .then(totalPkm.innerHTML = "Loading...")
             .then(resp => resp.json())
@@ -120,7 +120,7 @@ const createCard = (poke) => {
     const card = document.createElement('div')
     card.classList.add('flip-card')
 
-    let img = chosenView == 'gif' ? poke.sprites.versions['generation-v']['black-white'].animated['front_'+chosenForm] || poke.sprites.other.showdown['front_'+chosenForm] || poke.sprites.other.home['front_'+chosenForm] : poke.sprites['front_'+chosenForm] || poke.sprites.other.home['front_'+chosenForm]
+    let img = chosenView == 'animated' ? poke.sprites.versions['generation-v']['black-white'].animated['front_'+chosenForm] || poke.sprites.other.showdown['front_'+chosenForm] || poke.sprites.other.home['front_'+chosenForm] : poke.sprites['front_'+chosenForm] || poke.sprites.other.home['front_'+chosenForm]
     let name = poke.species['name']
     let number = poke.id.toString().padStart(4,'0')
     let pokeTypes = poke.types.map(typeInfo => typeInfo.type.name)
@@ -158,7 +158,7 @@ const createCard = (poke) => {
             </div>
         </div>
         <div class="back-card">
-            <h3>Abilities:</h3>${txtAbs}<br>
+            <h3>Abilities:</h3>${txtAbs}
             <h3>Base Stats:</h3>
             <ul>
                 <li><small>HP </small>${stats[0]} 💗</li>
